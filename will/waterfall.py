070d7be25acfd2eb6886ae1599950d03f367d0bb
William McDermott <will_mcdermott@me.com>
2020-03-02 23:41:51 +0000

    Update waterfall.py

--- will/waterfall.py
+++ will/waterfall.py
@@ -10,11 +10,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def waterfall(matrix, vector):
+def waterfall(non_d_matrix,vector,d,D):
+    
+    matrix = non_d_matrix*0.14
+    
+    y_axis = np.fliplr(vector)*4/30
 
     fig1, ax1 = plt.subplots()
     
-    wf = ax1.contourf(np.flip(np.transpose(matrix)),np.fliplr(vector))
+    wf = ax1.contourf(np.flip(np.transpose(matrix)),y_axis)
     ax1.set_title('Waterfall')
     ax1.set_xlabel('Distance')
     ax1.set_ylabel('Time')
